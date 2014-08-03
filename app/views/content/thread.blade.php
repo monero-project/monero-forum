4b39589fc8ee96d974be334abef4f62707a21317
EDVINAS PAULAUSKAS <edvinas@fluxministry.com>
2014-08-03 09:09:42 -0700

    Small traversal update

--- app/views/content/thread.blade.php
+++ app/views/content/thread.blade.php
@@ -23,32 +23,10 @@
 	</div>
 	{{ NULL; unset($posts[0]) }}
 	<div class="col-lg-12 replies-list">
-		<h3 class="pull-left">Replies: {{ $posts->count() }}</h3>
+		<h3 class="pull-left">Replies: {{ Thread::find($thread_id)->posts()->count() }}</h3>
 		<button class="btn btn-default pull-right">Load More</button>
 	</div>
-	
-	@foreach ($posts as $post)
-	<div id="post-{{ $post->id }}" class="post col-lg-12">
-			<h4>{{ $post->title }}</h4>
-			<p class="post-meta"><a href="/user/{{ $post->user->id }}" target="_blank">{{ $post->user->username }}</a> posted this on {{ $post->created_at }}</p>
-			{{ Markdown::string(e($post->body)) }}
-			<div class="btn-group btn-group-sm post-buttons">
-			  <button type="button" onclick="post_reply({{ $post->id }}, {{ $thread_id }}, '{{ $post->title }}')" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span>
- Reply</button>
- 			  @if ($post->user->id != Auth::user()->id)
-			  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-flag"></span>
- Flag</button>
- 			  @endif
-			  @if ($post->user->id == Auth::user()->id)
-			  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span>
- Edit</button>
-			  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash"></span>
- Delete</button>
-			  @endif
-			</div>
-	</div>
-	{{ display_children($post->id, $thread_id, 1) }}
-	@endforeach
+	{{ display_children(NULL, $thread_id, 0) }}
 	<hr>
 	@if(isset($errors) && sizeof($errors) > 0)
 	<div class="alert alert-danger alert-dismissible" role="alert">
