a63216b09dcdd8949cc695a449ed0f95ed751af9
SkepticalHippo <edvinas@fluxministry.com>
2015-02-21 15:28:24 +0000

    add resource IDs to forums and threads

--- app/controllers/ThreadsController.php
+++ app/controllers/ThreadsController.php
@@ -132,7 +132,7 @@ class ThreadsController extends \BaseController {
 			}
 
 	        Session::put('thread_id', $thread_id);
-			return View::make('content.thread', array('posts' => $posts['list'], 'links' => $posts['links'], 'thread' => $thread, 'title' => 'Monero | '.$thread->forum->name.' &raquo; '.$thread->name));
+			return View::make('content.thread', array('resource_id' => $thread_id,'posts' => $posts['list'], 'links' => $posts['links'], 'thread' => $thread, 'title' => 'Monero | '.$thread->forum->name.' &raquo; '.$thread->name));
 		}
 		else
 		{
