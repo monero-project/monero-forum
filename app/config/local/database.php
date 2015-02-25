bfa45a25fe2498e4e43ddbac2b89962570a5e15c
SkepticalHippo <edvinas@fluxministry.com>
2015-02-25 23:39:57 +0000

    monero environment

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
+			'database'  => 'monero',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
