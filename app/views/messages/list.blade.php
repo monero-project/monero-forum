4010affeb99f2b606476d9a8bf4575edd48ee8ca
SkepticalHippo <edvinas@fluxministry.com>
2015-03-15 16:40:43 +0000

    convo list improvements

--- app/views/messages/list.blade.php
+++ app/views/messages/list.blade.php
@@ -6,23 +6,44 @@
 			<a href="/messages/create"><button class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus"></span> New Conversation</button></a>
 		</div>
 	</div>
-	@if(count($conversations))
-		@foreach($conversations as $conversation)
-			<div class="row conversation-row">
-				<div class="col-md-8">
-					<a href="{{ URL::route('messages.conversation', [$conversation->id]) }}">{{{ $conversation->title  }}}</a> <small class="pull-right">{{ $conversation->created_at->diffForHumans() }}</small>
+	<div class="row category-block">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h1 class="panel-title"><span class="glyphicon glyphicon-envelope"></span> Private Messages</h1>
 				</div>
-				<div class="col-md-4 text-right">
-					<img class="profile-picture-sm" src="/uploads/profile/small_{{ $conversation->user->profile_picture }}"><a href="{{ URL::route('user.show', array($conversation->user->username)) }}">{{ $conversation->user->username }}</a>,
-					<img class="profile-picture-sm" src="/uploads/profile/small_{{ $conversation->receiver->profile_picture }}"><a href="{{ URL::route('user.show', array($conversation->receiver->username)) }}">{{ $conversation->receiver->username }}</a>
+				<div class="panel-body thread-list">
+					@foreach ($conversations as $conversation)
+						<div class="row">
+							<div class="col-md-6">
+									<span class="glyphicon glyphicon-envelope"></span>
+									<a class="thread-title" data-toggle="tooltip" data-placement="top" data-original-title="{{{ $conversation->title }}}" href="{{ URL::route('messages.conversation', [$conversation->id]) }}">
+										{{{ str_limit($conversation->title, 50, ' [...]') }}}
+									</a>
+							</div>
+							<div class="col-md-4">
+								<p>Participants:
+									<b><a class="board-meta" href="/user/{{ $conversation->user->username }}">{{ $conversation->user->username }}</a></b>
+									<b><a class="board-meta" href="/user/{{ $conversation->receiver->username }}">{{ $conversation->receiver->username }}</a></b>
+									, {{ $conversation->created_at }}
+								</p>
+							</div>
+							<div class="col-md-2 thread-replies">
+								<p>Messages: <b>{{ $conversation->messages->count() }}</b></p>
+							</div>
+						</div>
+					@endforeach
 				</div>
-			</div>
-		@endforeach
-	@else
-		<div class="row">
-			<div class="well">
-				You have no messages.
 			</div>
 		</div>
-	@endif
+		<div class="post-links">
+			{{ $conversations->links() }}
+		</div>
+@stop
+
+@section('javascript')
+	<script type="text/javascript">
+		$(function () {
+			$("[data-toggle='tooltip']").tooltip();
+		});
+	</script>
 @stop
