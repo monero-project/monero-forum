8e8f0faf7ad731d881552344c39df0c03a0b8662
SkepticalHippo <edvinas@fluxministry.com>
2015-04-25 14:26:48 +0100

    Removed debug code from editor.

--- public/js/bootstrap-markdown.js
+++ public/js/bootstrap-markdown.js
@@ -989,27 +989,22 @@
             // transform selection and set the cursor into chunked text
             if ((pointer = 3, content.substr(selected.start-pointer,pointer) === '## ')
                 || (pointer = 2, content.substr(selected.start-pointer,pointer) === '##')) {
-                console.log('heading present');
                 if (selected.start > 0 && (prevChar = content.substr(selected.start-1,1), !!prevChar && prevChar != '\n')) {
                     e.replaceSelection('#' + chunk);
                     cursor = selected.start + 1;
-                    console.log(1);
                 }
                 else {
                     // Empty string before element
                     e.replaceSelection('#'+chunk);
                     cursor = selected.start+1;
-                    console.log(2);
                 }
             } else if (selected.start > 0 && (prevChar = content.substr(selected.start-1,1), !!prevChar && prevChar != '\n' && prevChar != '#')) {
               e.replaceSelection('\n\n##'+chunk);
               cursor = selected.start+4;
-                console.log('elseif');
             } else {
               // Empty string before element
               e.replaceSelection('##'+chunk);
               cursor = selected.start+2;
-                console.log('lastelse');
             }
 
             // Set the cursor
