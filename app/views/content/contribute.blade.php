03af54c5f3ff54d0047d6dfca9c25d0d3b23d376
SkepticalHippo <edvinas@fluxministry.com>
2015-05-26 14:58:05 +0100

    Typo fix + address display.

--- app/views/content/contribute.blade.php
+++ app/views/content/contribute.blade.php
@@ -7,7 +7,7 @@
 			<p>In order to contribute to the cause of <strong>"{{{ $thread->name }}}"</strong> all you have to do is the following:</p>
 			<ol>
 				<li>Have a <strong>valid Monero address</strong>. If you don't have one, you can read on <a href="http://getmonero.org/getting-started/">getting started</a>!</li><br>
-				<li>Send he amount of XMR that you wish to contribute to the address:<br> <strong>{{ Config::get('monero::xmr_my_addr') }}</strong></li><br>
+				<li>Send the amount of XMR that you wish to contribute to the address:<br> <strong>{{ Config::get('monero::address') }}</strong></li><br>
 				<li>Make sure that you enter the payment ID to be <strong>{{ $thread->funding->payment_id }}</strong>!
 					Otherwise, we will not be able to assign your contribution to this specific project!</li><br>
 			</ol>
