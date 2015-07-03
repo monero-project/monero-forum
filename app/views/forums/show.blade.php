068dc914e99fbcb87dc816a8564b63f3afb95bc7
SkepticalHippo <edvinas@fluxministry.com>
2015-07-03 23:54:08 +0100

    Template restructure.

--- app/views/forums/show.blade.php
+++ app/views/forums/show.blade.php
@@ -21,29 +21,7 @@
   </div>
   <div class="panel-body thread-list">
     @foreach ($threads as $thread)
-    <div class="row">
-	    <div class="col-md-6">
-	    	@if ($thread->moved && $forum->id != $thread->forum->id)
-	    	<a class="thread-title" alt="{{{ $thread->name }}}" data-toggle="tooltip" data-placement="top" data-original-title="{{{ $thread->name }}}" href="/{{ $thread->forum->id }}/{{ $thread->forum->slug() }}/{{ $thread->id }}/{{ $thread->slug() }}"><small>{{{ str_limit('Moved to:'. $thread->forum->name, 50, ' [...]') }}}</small>
-	    	@else
-	    	    @if ( $thread->new_posts )
-	    	    <img data-toggle="tooltip" data-placement="top" data-original-title="{{{ $thread->unread_posts }}}" src="//static.getmonero.org/images/icon_thread_new.png">
-	            @else
-	    	    <img src="//static.getmonero.org/images/icon_thread.png">
-	    	    @endif
-	    	<a class="thread-title" data-toggle="tooltip" data-placement="top" data-original-title="{{{ $thread->name }}}" href="/{{ $forum->id }}/{{ $forum->slug() }}/{{ $thread->id }}/{{ $thread->slug() }}">
-	    	@endif {{{ str_limit($thread->name, 50, ' [...]') }}}</a>
-	    </div>
-		<div class="col-md-4">
-		<p>Author: <b><a class="board-meta" href="/user/{{ $thread->user->username }}">{{ $thread->user->username }}</a></b>, {{ $thread->created_at }}</p>
-		</div>
-		<div class="col-md-2 thread-replies">
-			<p>Replies: <b>{{ $thread->posts()->count() - 1}}</b></p>
-		</div>
-	    @if($thread->funding)
-	    @include('forums.includes.funding_block')
-		@endif
-	</div>
+    @include('forums.includes.thread')
 	@endforeach
   </div>
 </div>
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -16,36 +16,7 @@
 	  </div>
 	  <div class="panel-body">
 		  @foreach ($category->forums as $forum)
-		  @if (Visibility::check('forum', $forum->id))
-		  <div class="row forum-block">
-			  <div class="col-md-1">
-			  @if($forum->unread_posts > 0)
-			  	<i class="forum-icon-active fa fa-comments" data-toggle="tooltip" data-placement="top" data-original-title="{{{ $forum->unread_posts }}}" ></i>
-			  @else
-			    <i class="forum-icon-active fa fa-comments-o forum-read"></i>
-			  @endif
-			  </div>
-			  <div class="col-md-6 forum-info">
-				  <h4><a href="/{{ $forum->id }}/{{ $forum->slug() }}">{{ $forum->name }}</a></h4>
-				  <p>{{ $forum->description }}</p>	  
-			  </div>
-			  <div class="col-md-3 forum-post">
-			  	<p class="stats-post-body">
-			  	@if ($forum->latest_post())
-			  	<a class="board-meta" href="{{ $forum->latest_thread()->permalink() }}">{{ e(str_limit($forum->latest_thread()->name, 57, '...')) }}</a>
-			  	<br>
-				Replied By: <b><a class="board-meta" href="/user/{{{ $forum->latest_thread()->latest_post()->user->username or "" }}}">{{{ $forum->latest_thread()->latest_post()->user->username or "" }}}</a></b>
-			    <br>
-			   {{{ $forum->latest_thread()->latest_post()->created_at or "" }}}
-			  	@endif
-			  	</p>
-			  </div>
-			  <div class="col-md-2 forum-counts">
-			  	<p><b>{{ $forum->thread_count() }}</b> threads<br>
-			  	<b>{{ $forum->reply_count() }}</b> replies</p>
-			  </div>
-		  </div>
-		  @endif
+			@include('pages.includes.forum')
 		  @endforeach
 	  </div>
 	</div>
