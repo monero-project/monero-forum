2aed3457824571041e5764c1e269fa67b931e18a
SkepticalHippo <edvinas@fluxministry.com>
2015-07-10 20:58:36 +0100

    Remove escaping HTML entities from PM email.

--- app/views/emails/pm.blade.php
+++ app/views/emails/pm.blade.php
@@ -1,6 +1,6 @@
 <p>Hello {{{ $receiver->username }}},</p>
 <p>You have a new private message on the Monero Forum from {{{ $sender->username  }}}:</p>
-<p><b>{{{ str_limit($pm->conversation->title, 30, '[...]') }}}</b></p>
+<p><b>{{ str_limit($pm->conversation->title, 30, '[...]') }}</b></p>
 {{ Markdown::string(e($pm->body)) }}
 <p><a href="{{ URL::route('messages.conversation', [$pm->conversation_id]) }}">Click here to read and reply to this message</a>.</p>
 <p>Regards,</p>
