5d32be7c066b8a2ab53f95674c882498fe0e1ec3
SkepticalHippo <edvinas@fluxministry.com>
2015-08-19 01:20:58 +0100

    Remove escaping from emails.

--- app/views/emails/mention.blade.php
+++ app/views/emails/mention.blade.php
@@ -1,7 +1,7 @@
 <p>Hello {{ $username }},</p>
 <p>You have been mentioned in a post by {{{ $mentioned_by }}}:</p>
 <p><b>{{{ str_limit($title, 30, '[...]') }}}</b></p>
-{{ Markdown::string(e($body)) }}
+{{ $body }}
 <p>There may be more replies this thread received after this email notification. <a href="{{ URL::route('thread.short', [$thread_id]) }}">Click here to visit the thread</a>.</p>
 <p>Regards,</p>
 <p>The Monero Project Team</p>
