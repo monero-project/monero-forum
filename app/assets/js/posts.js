5b86eb816174ba985d1f12e1c6c8c52a0698fe52
Alin Ionut Ghinoiu <alin@agvision.ro>
2016-09-16 14:06:09 +0300

    [MON-20] Disable jQuery hide() for user posts history pagination

--- app/assets/js/posts.js
+++ app/assets/js/posts.js
@@ -40,7 +40,8 @@ function init() {
     $('.drawer-button').show();
     $('.reply-cancel').show();
     $('.hide').hide();
-    $('.pagination').hide();
+    //hide pagination on all pages except user post history
+    if (!$('.pagination-user-posts').length) $('.pagination').hide();
     $('.replies-list b').show();
     $('.content-control').show();
     $('.hidden-post-content').hide();
