b05ebeb935b6248f189acd76a3d32a09e3408207
Alin Ionut Ghinoiu <alin.ghinoiu@gmail.com>
2016-10-07 20:22:33 +0300

    [MON-18] Redirect notifications to login if not authenticated

--- app/controllers/NotificationsController.php
+++ app/controllers/NotificationsController.php
@@ -5,18 +5,22 @@ class NotificationsController extends \BaseController
 
 	public function getIndex()
 	{
-		$user = Auth::user();
-		$notifications = $user->notifications()->orderBy('created_at', 'DESC')->paginate(20);
+		if (Auth::check()) {
+			$user = Auth::user();
+			$notifications = $user->notifications()->orderBy('created_at', 'DESC')->paginate(20);
 
-		//some black magic so that we can fire an event after the HTML is rendered.
-		//will let us show that there indeed are new notifications.
-		$view = View::make('notifications.index', compact('notifications'));
+			//some black magic so that we can fire an event after the HTML is rendered.
+			//will let us show that there indeed are new notifications.
+			$view = View::make('notifications.index', compact('notifications'));
 
-		$view = Response::make($view);
+			$view = Response::make($view);
 
-		$user->notifications()->where('is_new', 1)->update(['is_new' => 0]);
+			$user->notifications()->where('is_new', 1)->update(['is_new' => 0]);
 
-		return $view;
+			return $view;
+		} else {
+			return Redirect::to('/login');
+		}
 	}
 
 	//shows the notifications count.
